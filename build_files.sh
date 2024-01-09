9538474c128ff142a699f6e40e69256d985e0805
Avishkar <avishkar2108@gmail.com>
2024-01-09 18:29:43 +0530

    --noinput for vercel deploy

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+echo  "START BUILD"
+python2.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo  "END BUILD"
